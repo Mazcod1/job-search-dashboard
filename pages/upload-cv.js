32ff15d18dc7302188813fc82d98178740b4fac1
Mazcod1 <111071991+Mazcod1@users.noreply.github.com>
2025-09-05 20:19:00 +0300

    Create upload-cv.js

---
+++ pages/upload-cv.js
@@ -0,0 +1,9 @@
+export default function UploadCV() {
+  return (
+    <div>
+      <h1>Upload Your CV</h1>
+      <input type="file" />
+      <p style={{ color: "#64748b" }}>Supported formats: PDF, DOCX</p>
+    </div>
+  );
+}
