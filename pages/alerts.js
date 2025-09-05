9a612a196202cc18c136f90e9dcbf22c4e8a8aac
Mazcod1 <111071991+Mazcod1@users.noreply.github.com>
2025-09-05 20:20:20 +0300

    Create alerts.js

---
+++ pages/alerts.js
@@ -0,0 +1,8 @@
+export default function Alerts() {
+  return (
+    <div>
+      <h1>Alerts & Settings</h1>
+      <p>Here you’ll configure email / Telegram notifications for new job postings.</p>
+    </div>
+  );
+}
